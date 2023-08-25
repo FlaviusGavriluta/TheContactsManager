305ff95166afc8c7c2e1207ececdc4e651871931
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-25 14:21:48 +0300

    create two methods

--- src/main/java/org/example/ContactsManager.java
+++ src/main/java/org/example/ContactsManager.java
@@ -5,8 +5,23 @@ public class ContactsManager {
     Contact[] myFriends;
     int friendsCount;
 
+    // Constructor:
     public ContactsManager() {
         this.myFriends = new Contact[500];
         this.friendsCount = 0;
+    }
+
+    // Methods:
+    void addContact(Contact contact) {
+        myFriends[friendsCount] = contact;
+        friendsCount++;
+    }
+
+    Contact searchContact(String searchName) {
+        for (int i = 0; i < friendsCount; i++) {
+            if (myFriends[i].name.equals(searchName))
+                return myFriends[i];
+        }
+        return null;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
         // Create a new object of type ContactsManager:
         ContactsManager manager = new ContactsManager();
-        manager.friendsCount = 0;
-        manager.myFriends[0] = new Contact();
-        manager.myFriends[0].name = "John";
-        manager.myFriends[0].phoneNumber = "0012223333";
-        manager.myFriends[0].email = "John@gmail.com";
-        manager.friendsCount++;
-        manager.myFriends[1] = new Contact();
-        manager.myFriends[1].name = "Tom";
-        manager.myFriends[1].phoneNumber = "0321123091";
-        manager.myFriends[1].email = "Tom@gmail.com";
-        manager.friendsCount++;
-        for (int i = 0; i < manager.friendsCount; i++) {
-            System.out.println(manager.myFriends[i].name);
-        }
     }
 }
