f9174e0c8f1c5f9006b35bffdeb7479dd49e4478
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-25 19:32:20 +0300

    added a constructor and refactor the main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,12 @@ public class Main {
     public static void main(String[] args) {
         // Create a new object of type ContactsManager:
         ContactsManager myContactsManager = new ContactsManager();
-        Contact andy = new Contact();
-        andy.name = "Andy";
-        andy.email = "Andy@email.com";
-        andy.phoneNumber = "0040234578";
 
-        Contact tom = new Contact();
-        tom.name = "Tom";
-        tom.email = "Tom@email.com";
-        tom.phoneNumber = "0040233578";
-
-        Contact john = new Contact();
-        john.name = "John";
-        john.email = "John@email.com";
-        john.phoneNumber = "0041234578";
-
-        Contact ema = new Contact();
-        ema.name = "Ema";
-        ema.email = "Ema@email.com";
-        ema.phoneNumber = "0030234578";
+        // Create new Contacts object:
+        Contact andy = new Contact("Andy", "Andy@email.com", "0040234578");
+        Contact tom = new Contact("Tom", "Tom@email.com", "0040233578");
+        Contact john = new Contact("John", "John@email.com", "0041234578");
+        Contact ema = new Contact("Ema", "Ema@email.com", "0030234578");
 
         myContactsManager.addContact(andy);
         myContactsManager.addContact(tom);
@@ -33,7 +20,7 @@ public class Main {
         if (foundContact != null)
 
 
-            System.out.println("Contact found: " + foundContact.name + ", " + foundContact.phoneNumber);
+            System.out.println("Contact found: " + foundContact.name + ", phone: " + foundContact.phoneNumber);
         else
             System.out.println("Contact not found!");
         System.out.println("_____________________");
